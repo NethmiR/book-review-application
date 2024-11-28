739b5714998f6ac05c619d66b876264aad472ccc
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-11-29 01:37:35 +0530

    feat/ updated the app.js

--- book-review-app-server/app.js
+++ book-review-app-server/app.js
@@ -5,12 +5,15 @@ const bookRoutes = require('./routes/bookRoutes');
 const authorRoutes = require('./routes/authorRoutes'); // Import author routes
 const authRoutes = require('./routes/authRoutes'); // Import auth routes
 const reviewRoutes = require('./routes/reviewRoutes'); // Import review routes
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+app.use(cors());
 
 // Define routes
 app.use('/api/user', userRoutes);
@@ -19,15 +22,10 @@ app.use('/api/author', authorRoutes); // Add author routes
 app.use('/api/auth', authRoutes); // Add auth routes
 app.use('/api/review', reviewRoutes); // Add review routes
 
-// Sync database and start the server
-// sequelize.sync({ alter: true }).then(() => {
-//     app.listen(port, () => {
-//         console.log(`Server is running at http://localhost:${port}`);
-//     });
-// }).catch(err => {
-//     console.error('Unable to sync the database:', err);
-// });
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+sequelize.sync({ alter: true }).then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}).catch(err => {
+    console.error('Unable to sync the database:', err);
 });
